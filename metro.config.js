af0970f8c10f4a4793ef47a95c86e59bda2c2ec5
sapnasawai <sapnasawai1999@gmial.com>
2025-06-29 18:46:43 +0530

    react-native-animated config done

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +7,9 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
+const defaultConfig = getDefaultConfig(__dirname);
 const config = {};
+const mergedConfig = mergeConfig(defaultConfig, config);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = wrapWithReanimatedMetroConfig(mergedConfig);
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
