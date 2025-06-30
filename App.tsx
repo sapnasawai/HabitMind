8ef6aa0f82ba46d67df9d4f8b2e6617876b614bf
sapnasawai <sapnasawai199@gmail.com>
2025-06-30 23:50:04 +0530

    tab navigation

--- App.tsx
+++ App.tsx
@@ -2,15 +2,19 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Text, TouchableOpacity, View} from 'react-native';
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import ProgressScreen from './src/screens/ProgressScreen';
+import HabbitsScreen from './src/screens/HabbitsScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function RootStack() {
   return (
     <Stack.Navigator
-      initialRouteName="Home"      
+      initialRouteName="Home"
       screenOptions={{
         headerStyle: {backgroundColor: 'tomato'},
       }}>
@@ -24,10 +28,22 @@ function RootStack() {
   );
 }
 
+function BottomTabRootStack() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+       <Tab.Screen name="Habbits" component={HabbitsScreen} />
+      <Tab.Screen name="Progress" component={ProgressScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+     
+    </Tab.Navigator>
+  );
+}
 export default function App() {
   return (
     <NavigationContainer>
-      <RootStack />
+      {/* <RootStack /> */}
+      <BottomTabRootStack/>
     </NavigationContainer>
   );
 }
